5be4df50e7e14a420e02a460342ad07514607304
rhkhan <rhkhan.rubel@gmail.com>
2020-02-23 09:42:57 +0600

    Update BinarySearchIterativeWay.cs

--- BinarySearchIterativeWay.cs
+++ BinarySearchIterativeWay.cs
@@ -4,15 +4,15 @@ public static int BinarySearchIterative(int[] arr,int keyVal){
   int minIndex=0;
   int maxIndex=arr.Length-1;
 
-  while(minIndex<=maxIndex){
+  while(minIndex<=maxIndex)
+  {
       int midIndex=(minIndex+maxIndex)/2;
       if(keyVal==arr[midIndex])
         return ++midIndex;
-      else if(keyVal>midIndex){
+      else if(keyVal>midIndex)
         minIndex=midIndex+1; // If keyVal is greater then the mid the search in the right portion
       else
-        maxIndex=midIndex-1; // else search in the left portion
-  }
+        maxIndex=midIndex-1; // else search in the left portion  
 }
     return -1;
 }
