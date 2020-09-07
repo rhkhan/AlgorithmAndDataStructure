ae892871e5ebb3a131e0eb8818e7f03881fcbf65
rhkhan <rhkhan.rubel@gmail.com>
2020-09-07 15:22:26 +0600

    Find pairs with given sum in doubly linked list
    Given a sorted doubly linked list of positive distinct elements, the task is to find pairs in doubly linked list whose sum is equal to given value x, without using any extra space ?
    Example:
    
    Input : head : 1 <-> 2 <-> 4 <-> 5 <-> 6 <-> 8 <-> 9
            x = 7
    Output: (6, 1), (5,2)

--- LinkedListDoublyPairSum.cs
+++ LinkedListDoublyPairSum.cs
@@ -1,3 +1,4 @@
+// Solution: O(n^2)
 public void printSumPairs(Node head,int x)
 {
   if(head==null || head.next==null)
@@ -20,6 +21,36 @@ public void printSumPairs(Node head,int x)
       } // end inner while loop
     }
   } // end outer while loop  
-}
+} // O(n^2) solution
 
-// O(n^2) solution
+
+// Solution: O(n)
+public void pairSum( Node head, int x)  
+    {  
+        Node first = head;  
+        Node second = head;  
+        while (second.next != null)  
+            second = second.next;  
+   
+        bool found = false;  
+        while (first != null && second != null &&  first != second && second.next != first)  
+        {  
+            if ((first.data + second.data) == x)  
+            {  
+                found = true;  
+                Console.WriteLine( "(" + first.data +  ", "+ second.data + ")" );  
+  
+                first = first.next;  
+                second = second.prev;  
+            }  
+            else
+            {  
+                if ((first.data + second.data) < x)  
+                    first = first.next;  
+                else
+                    second = second.prev;  
+            }  
+        }  
+        if (found == false)  
+            Console.WriteLine("No pair found");  
+    }  //Time compexity: O(n), Auxiliary space : O(1)
