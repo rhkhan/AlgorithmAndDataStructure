6aba8adfd6738326013da2f46ec8a4602ab7b700
rhkhan <rhkhan.rubel@gmail.com>
2020-09-15 15:54:54 +0600

    BinaryMinHeap
    Building a min heap

---
+++ MinHeap.cs
@@ -0,0 +1,99 @@
+using system;
+
+public class BinaryMinHeap{
+  int[] data;
+  int heapsize;
+  
+  public BinaryMinHeap(int size){
+    data=new int[size];
+    heapsize=0;
+  }
+  
+  public int getMinimum(){
+    if(heapsize==0)
+      throw new KeyNotFoundException();
+    else
+      return data[0];
+  }
+  
+  public int  getLeftChildIndex(int index){
+    return 2*index+1;
+  }
+  
+  public int getRightChildIndex(int index){
+    return 2*index+2;
+  }
+  
+  public int getParentIndex(int index){
+    return (index-1)/2;
+  }
+  
+  public void insert(int item){
+    if(heapsize>=data.Length)
+      throw new IndexOutOfRangeException();
+      
+    data[heapsize]=item;
+    heapifyUP(heapsize);
+    heapsize++;
+  }
+  
+  public void heapifyUP(int index){
+    if(index==0)
+     return;
+    
+    int parentIndex=getParentIndex(index);
+    if(data[parentIndex]>=data[index]){
+      int tmp=data[parentIndex];
+      data[parentIndex]=data[index];
+      data[index]=tmp;
+      heapifyUP(parentIndex);
+    }
+  }
+  
+  public void removeMin(){
+    if(heapsize==0)
+      throw new KeyNotFoundException();
+    else{
+      data[0]=data[heapsize-1];
+      if(heapsize>0)
+        heapifyDOWN(0);
+      heapsize--;
+    }
+  }
+  
+  public void heapifyDOWN(int index){
+    int left=getLeftIndex(index);
+    int right=getRightIndex(index);
+    
+    if(right>=heapsize){
+      if(left>=heapsize)
+       return;
+      else
+       smallest=left;
+    }
+    else{
+      if(data[left]<=data[right]
+       smallest=left;
+      else
+       smallest=right;
+    }
+   if(data[index]>data[smallest]){
+     int tmp=data[index];
+     data[index]=data[smallest];
+     data[smallest]=tmp;
+     heapifyDOWN(smallest);
+   }
+  }
+}
+
+//Driver function
+public static void main(string[] args){
+ BinaryMinHeap bmp=new BinaryMinHeap();
+ bmp.insert(1);
+ bmp.insert(2);
+ bmp.insert(3);
+ Console.WriteLine(bmp.getMinimum());
+ bmp.removeMin();
+ Console.WriteLine(bmp.getMinimum());
+ Console.Read();
+}
