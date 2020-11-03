4a95d24e72e1088f9a9bc0071ce7697cb26789dd
rhkhan <rhkhan.rubel@gmail.com>
2020-11-03 11:50:29 +0600

    Dijkstra shortest path algorithm
    Inline comments added

--- DijkstraShortestPath.cs
+++ DijkstraShortestPath.cs
@@ -26,66 +26,67 @@ namespace DijkstraAlgorithm
                                 {0, 0, 2, 0, 0, 0, 6, 7, 0}
                             };
 
-            Dijkstra(graph, 5);
+            Dijkstra(graph, 0);
 
             Console.Read();
         }
 
 
-        
-        static void Dijkstra(int[,] grpahArray, int source) { 
-        
-            int[] adjacentArray=new int[V];
-            int[] trackAdjacent = new int[V];
-            int[] sptArray = new int[V];
+
+        static void Dijkstra(int[,] grpahArray, int source)
+        {
+
+            int[] adjacentArray = new int[V]; // to store the shortest distance value from the source to all nodes
+            int[] trackAdjacent = new int[V]; // to track the min path from the recent source
+            int[] sptArray = new int[V]; // to track if all the nodes have been checked
+            
+            int originalSource = source;
 
             for (int i = 0; i < V; i++)
                 adjacentArray[i] = INF;
-            adjacentArray[source] = 0;
+            adjacentArray[source] = 0; // From source to source, distance is always zero
 
 
-            
-            sptArray[0] = 0;
-            int sptIndex=1;
-            int currentSource = 0;
-            
 
-            for (int i = 0; i < V; i++)
+            sptArray[source] = 0; // initial source vertex is tracked at the first place
+            int sptIndex = 1;
+            int currentSource = source;
+
+
+            for (int i = currentSource; i < V; i++) // loop for source
             {
-                int track=0;
-                trackAdjacent = new int[V];
+                trackAdjacent = new int[V]; // to get minimum distance value from the current source
                 for (int val = 0; val < trackAdjacent.Length; val++)
                     trackAdjacent[val] = INF;
 
-                    for (int j = 0; j < V; j++)
+                for (int j = 0; j < V; j++) // loop from source to available valid adjacent nodes can be reached
+                {
+                    if (grpahArray[i, j] > 0 && !sptArray.Contains(j)) // If adjacent node can be reached and not allready considered
                     {
-                        if (grpahArray[i, j] > 0 && !sptArray.Contains(j))
+                        if (adjacentArray[j] > (adjacentArray[source] + grpahArray[i, j]))
                         {
-                            track = j;
-
-                            if (adjacentArray[j] > (adjacentArray[source] + grpahArray[i, j]))
-                            {
-                                adjacentArray[j] = adjacentArray[source] + grpahArray[i, j]; // update adjacent list
-                                trackAdjacent[track] = adjacentArray[j];
-                            }
-                            else{
-                                trackAdjacent[track] = adjacentArray[j];
-                            }
-
-                            int minValue = trackAdjacent.Min();
-                            int p = Array.IndexOf(trackAdjacent, minValue);
-
-
-                            // after update operation select the minimum weighted node as the destination to travel from the current source
-                            // and make the travelled destination as the new source
-
-                            currentSource = p;
-
+                            adjacentArray[j] = adjacentArray[source] + grpahArray[i, j]; // update adjacent list
+                            trackAdjacent[j] = adjacentArray[j];
                         }
+                        else
+                        {
+                            trackAdjacent[j] = adjacentArray[j];
+                        }
+
+                        int minValue = trackAdjacent.Min();
+                        int p = Array.IndexOf(trackAdjacent, minValue); // get index of the minimum vertex
+
+
+                        // after update operation select the minimum weighted node as the destination to travel from the current source
+                        // and make the travelled destination as the new source
+
+                        currentSource = p; // set the current souce to the minimum distance valued vertex
+
                     }
+                }
 
                 source = currentSource;// update the source in the adjacent array
-                i = source-1;
+                i = source - 1;
                 sptArray[sptIndex] = currentSource;
                 sptIndex++;
                 if (sptIndex > sptArray.Length - 1)
@@ -94,8 +95,9 @@ namespace DijkstraAlgorithm
 
             Console.WriteLine("Dijkstra Algorithm: Distance from source vertex to destination vertex---");
             Console.WriteLine();
-            Console.WriteLine("Vertex  Distance from Source");
-            for (int i = 0; i < adjacentArray.Length; i++) {
+            Console.WriteLine("Destination   Distance from Source ("+originalSource+")");
+            for (int i = 0; i < adjacentArray.Length; i++)
+            {
                 Console.WriteLine(i + "              " + adjacentArray[i]);
             }
 
