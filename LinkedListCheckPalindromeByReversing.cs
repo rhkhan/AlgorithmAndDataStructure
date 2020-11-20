d3ce843905a034e3bd5a4d81b998a8fef465bc00
rhkhan <rhkhan.rubel@gmail.com>
2020-11-20 11:57:15 +0600

    Check palindrome

---
+++ LinkedListCheckPalindromeByReversing.cs
@@ -0,0 +1,7 @@
+/*
+This method takes O(n) time and O(1) extra space. 
+  1) Get the middle of the linked list. 
+  2) Reverse the second half of the linked list. 
+  3) Check if the first half and second half are identical. 
+  4) Construct the original linked list by reversing the second half again and attaching it back to the first half
+*/
