b99e0da29c7239cafb93a7d33a9355ac8a840ec9
rhkhan <rhkhan.rubel@gmail.com>
2020-11-23 13:16:15 +0600

    Insert in a sorted Linked List
    Insert in a sorted Linked List

---
+++ LinkedListSortedInsert.cs
@@ -0,0 +1,5 @@
+/*
+  1. if head data is greater then the new_node data, then insert as head.
+  2. if new_node data is greater then current node data and less then next node data then insert into the middle
+  3. or insert new_node to the last.
+*/
