c57cb9ae4ed56c42ffda37e39f8cd442f0f9f29e
rhkhan <rhkhan.rubel@gmail.com>
2020-11-23 13:09:57 +0600

    Delete N nodes after M nodes of a linked list
    Delete N nodes after M Nodes of a linked list

---
+++ LinkedListDeletNafterM.cs
@@ -0,0 +1,9 @@
+void DeleteNnodesAfterMnodes(){
+  // initialize a counterM for M nodes
+  // Traverse upto M nodes
+  // store the node after M nodes into a variable
+  // set the nodes next to null after traversing the M node
+  // initialize another counterN for N nodes
+  // traverse the node stored earlier upto N nodes
+  // finally set the previously traversed M nodes next to the N nodes next pointer
+ }
