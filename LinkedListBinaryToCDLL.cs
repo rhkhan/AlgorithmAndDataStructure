6ec71d0e83a6fcbdefba6f6a8407c88e748b9e72
rhkhan <rhkhan.rubel@gmail.com>
2020-12-01 16:06:26 +0600

    Convert a Binary Tree to a CDLL
    Given a Binary Tree, convert it to a Circular Doubly Linked List (In-Place).

--- LinkedListBinaryToCDLL.cs
+++ LinkedListBinaryToCDLL.cs
@@ -7,3 +7,59 @@ Approach 1:
  - if node is not the head then set next and prev pointer
  - if node is last the set the next to head and prev to the previous node
 */
+
+
+//Approach 2:
+public class Tree
+{
+  public Node binaryTreetoCDLL(Node root)
+  {
+   if(root==null)
+    return null;
+
+    Node leftList=binaryTreetoCDLL(root.left);
+    Node rightList=binaryTreetoCDLL(root.right);
+
+    root.left=root.right=root;
+
+    return concatenate(concatenate(leftList,root),rightList));
+  }
+
+  public Node concatenate(Node leftList,Node rightList)
+  {
+    if(leftList==null)
+     return rightList;
+    if(rightList==null)
+     return leftList;
+
+     Node leftLast=leftList.left; // leftList previous pointer points to the last node of the left list
+     Node rightLast=rightList.left;
+
+     //concating
+     leftLast.right=rightList;
+     rightList.left=leftLast;
+
+     //adjusting the pointer for the circular pointing
+     leftList.left=rightLast;
+     rightLast.right=leftList
+
+     return leftList;
+  }
+}
+public class Program 
+{ 
+    public static void Main(string[] args) 
+    { 
+        // Build the tree  
+        Tree tree = new Tree(); 
+        tree.root = new Node(10); 
+        tree.root.left = new Node(12); 
+        tree.root.right = new Node(15); 
+        tree.root.left.left = new Node(25); 
+        tree.root.left.right = new Node(30); 
+        tree.root.right.left = new Node(36); 
+  
+        // head refers to the head of the Link List  
+        Node head = tree.binaryTreetoCDLL(tree.root); 
+    } 
+} 
