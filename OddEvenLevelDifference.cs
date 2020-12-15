a58dd499d6dbd2931ea68cf5bab6e062e71e4ff0
rhkhan <rhkhan.rubel@gmail.com>
2020-12-15 16:58:10 +0600

    Difference between odd level and even level sum
    Difference between sums of odd level and even level nodes of a Binary Tree
    
    Given a a Binary Tree, find the difference between the sum of nodes at odd level and the sum of nodes at even level. Consider root as level 1, left and right children of root as level 2 and so on.
    
    For example, in the following tree, sum of nodes at odd level is (5 + 1 + 4 + 8) which is 18. And sum of nodes at even level is (2 + 6 + 3 + 7 + 9) which is 27. The output for following tree should be 18 – 27 which is -9.
    
          5
        /   \
       2     6
     /  \     \  
    1    4     8
        /     / \ 
       3     7   9

--- OddEvenLevelDifference.cs
+++ OddEvenLevelDifference.cs
@@ -4,3 +4,32 @@
    update the oddLevelSum for all the odd leveled nodes and evenLevelSum for all the even leveled nodes
    calculate the difference and return the result
 */
+
+public int oddEvenLevelDiff(Node root)
+{
+  if(root==null)
+   return 0;
+  Queue<Node> queue=new Queue<Node>();
+  queue.Enqueue(root);
+  int oddLevelSum,evenLevelSum=0;
+  int level=1;
+  while(queue.Count>0)
+  {
+    int queueSize=queue.Count;
+    while(queueSize-->0)
+    {
+       Node tmp=queue.Dequeue();
+       if(level%2==0)
+        evenLevelSum+=tmp.data;
+       else
+        oddLevelSum+=tmp.data;
+        
+       if(tmp.left!=null)
+        queue.Enqueue(tmp.left);
+       if(tmp.right!=null)
+        queue.Enqueue(tmp.right);
+    }
+      level++;
+  }
+  return oddLevelSum-evenLevelSum;
+}
