a02c4de2e20eec652981b259c5b4c51782e3ffba
rhkhan <rhkhan.rubel@gmail.com>
2021-01-01 15:33:10 +0600

    Print Left View of a Binary Tree
    Given a Binary Tree, print left view of it. Left view of a Binary Tree is set of nodes visible when tree is visited from left side.
    
    Input : 
                     1
                   /   \
                  2     3
                 / \     \
                4   5     6             
    Output : 1 2 4
    Input :
            1
          /   \
        2       3
          \   
            4  
              \
                5
                 \
                   6
    Output :1 2 4 5 6

---
+++ LeftViewBinaryTree.cs
@@ -0,0 +1,47 @@
+public class BinaryTree
+{
+  public Node root;
+  public static int max_level=0;
+  
+  public virtual void printLeftView(Node root,int level) //Recursive way
+  {
+    if(root==null)
+     return;
+    if(level>max_level)
+    {
+       Console.write(root.data+" ");
+       max_level=level;
+    }
+    printLeftView(root.left,level+1);
+    printLeftView(root.right,level+1);
+  }
+  
+  public virtual void printLeftViewIterative(Node root,int level) //Iterative using queue
+  {
+    if(root==null)
+     return;
+    Queue<Node> q=new Queue<Node>();
+    queue.Enqueue(root);
+    while(queue.Count>0)
+    {
+      int n=queue.Count;
+      for(int i=1;i<=n;i++)
+      {
+        Node temp=queue.Dequeue();
+        if(i==n)
+        {
+         Console.Write(temp.data+" ");
+        }
+        if(temp.left!=null)
+         queue.Enqueue(temp.left);
+        if(temp.right!=null)
+         queue.Enqueue(temp.right);
+      }
+    }
+  }
+  
+  public static void Main()
+  {
+    printLeftView(root,1);
+  }
+}
