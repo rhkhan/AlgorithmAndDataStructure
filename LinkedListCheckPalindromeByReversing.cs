da2df894e751c192398b9201beb14ae96dfcb1a5
Rashedul Hossain Khan <rhkhan.rubel@gmail.com>
2022-05-12 10:04:16 +0600

    Added an additional step at the top

--- LinkedListCheckPalindromeByReversing.cs
+++ LinkedListCheckPalindromeByReversing.cs
@@ -1,7 +1,8 @@
 /*
-This method takes O(n) time and O(1) extra space. 
-  1) Get the middle of the linked list. 
-  2) Reverse the second half of the linked list. 
-  3) Check if the first half and second half are identical. 
-  4) Construct the original linked list by reversing the second half again and attaching it back to the first half
+This method takes O(n) time and O(1) extra space.
+  1) count the number of nodes in linkedlist.
+  2) Get the middle of the linked list. 
+  3) Reverse the second half of the linked list. 
+  4) Check if the first half and second half are identical. 
+  5) Construct the original linked list by reversing the second half again and attaching it back to the first half
 */
