79c9e4e30c2552fda5f6a3fea65cdeaf064833a3
Rashedul Hossain Khan <rhkhan.rubel@gmail.com>
2022-05-18 05:50:30 +0600

    fixed missing closing paranthesis and semicolon

--- BalancedParanthesis.cs
+++ BalancedParanthesis.cs
@@ -4,10 +4,11 @@ public bool isBalanced(string str)
   for(int i=0;i<str.Length;i++)
   {
     if(str[i]=='(' || str[i]=='{' || str[i]=='[')
-      st.push(str[i]
+      st.push(str[i]);
     else
     {
-      if(st.Count==0) return false;
+      if(st.Count==0) 
+        return false;
       else
       {
         if(str[i]==')' && st.Pop()!='(')
