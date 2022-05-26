7dae5761940806966996d2a39f5182af6f1e5e2d
Rashedul Hossain Khan <rhkhan.rubel@gmail.com>
2022-05-26 06:51:42 +0600

    Code extended

--- PrintAllKDistanceNode.cs
+++ PrintAllKDistanceNode.cs
@@ -1,43 +1,133 @@
-public virtual int printKDistanceNode(Node node, Node target,int k)
+using System;
+
+// C# program to print all nodes at a distance k from given node
+
+// A binary tree node
+public class Node
 {
-  if(root==null)
-   return -1;
-   
-   if(node==target)
-   {
-    printKDistanceDown(node,k);
-    return 0;
-   }
-    
-   int dl=printKDistanceNode(node.left,target,k)
-   if(dl!=-1)
-   {
-     if(dl+1==k)
-     {
-       Console.Write(node.data); 
-       Console.WriteLine(""); 
-     }
-     
-    else
-    {
-      printKDistanceNodeDown(node.right,k-dl-2);
-    }
-    return dl+1;
-   }
-   
-       int dr = printkdistanceNode(node.right, target, k); 
-  if (dr != -1) 
-  { 
-     if (dr + 1 == k) 
-     {   
-         Console.Write(node.data); 
-         Console.WriteLine(""); 
-      } 
-      else
-      { 
-        printkdistanceNodeDown(node.left, k - dr - 2); 
-      } 
-    return 1 + dr; 
-  } 
- return -1; 
+	public int data;
+	public Node left, right;
+
+	public Node(int item)
+	{
+		data = item;
+		left = right = null;
+	}
 }
+
+public class BinaryTree
+{
+	public Node root;
+	/* Recursive function to print all the nodes at distance k in
+	tree (or subtree) rooted with given root. */
+
+	public virtual void printkdistanceNodeDown(Node node, int k)
+	{
+		// Base Case
+		if (node == null || k < 0)
+		{
+			return;
+		}
+
+		// If we reach a k distant node, print it
+		if (k == 0)
+		{
+			Console.Write(node.data);
+			Console.WriteLine("");
+			return;
+		}
+
+		// Recur for left and right subtrees
+		printkdistanceNodeDown(node.left, k - 1);
+		printkdistanceNodeDown(node.right, k - 1);
+	}
+
+	// Prints all nodes at distance k from a given target node.
+	// The k distant nodes may be upward or downward.This function
+	// Returns distance of root from target node, it returns -1
+	// if target node is not present in tree rooted with root.
+	public virtual int printkdistanceNode(Node node, Node target, int k)
+	{
+		// Base Case 1: If tree is empty, return -1
+		if (node == null)
+		{
+			return -1;
+		}
+
+		// If target is same as root. Use the downward function
+		// to print all nodes at distance k in subtree rooted with
+		// target or root
+		if (node == target)
+		{
+			printkdistanceNodeDown(node, k);
+			return 0;
+		}
+
+		// Recur for left subtree
+		int dl = printkdistanceNode(node.left, target, k);
+
+		// Check if target node was found in left subtree
+		if (dl != -1)
+		{
+
+			// If root is at distance k from target, print root
+			// Note that dl is Distance of root's left child from
+			// target
+			if (dl + 1 == k)
+			{
+				Console.Write(node.data);
+				Console.WriteLine("");
+			}
+
+			// Else go to right subtree and print all k-dl-2 distant nodes
+			// Note that the right child is 2 edges away from left child
+			else
+			{
+				printkdistanceNodeDown(node.right, k - dl - 2);
+			}
+
+			// Add 1 to the distance and return value for parent calls
+			return 1 + dl;
+		}
+
+		// MIRROR OF ABOVE CODE FOR RIGHT SUBTREE
+		// Note that we reach here only when node was not found in left
+		// subtree
+		int dr = printkdistanceNode(node.right, target, k);
+		if (dr != -1)
+		{
+			if (dr + 1 == k)
+			{
+				Console.Write(node.data);
+				Console.WriteLine("");
+			}
+			else
+			{
+				printkdistanceNodeDown(node.left, k - dr - 2);
+			}
+			return 1 + dr;
+		}
+
+		// If target was neither present in left nor in right subtree
+		return -1;
+	}
+
+	// Driver program to test the above functions
+	public static void Main(string[] args)
+	{
+		BinaryTree tree = new BinaryTree();
+
+		/* Let us construct the tree shown in above diagram */
+		tree.root = new Node(20);
+		tree.root.left = new Node(8);
+		tree.root.right = new Node(22);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(12);
+		tree.root.left.right.left = new Node(10);
+		tree.root.left.right.right = new Node(14);
+		Node target = tree.root.left.right;
+		tree.printkdistanceNode(tree.root, target, 2);
+	}
+}
+
+// This code is contributed by Shrikant13
